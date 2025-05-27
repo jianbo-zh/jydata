6292f2dee8215784820c273473e426967fb8fe19
jianbo-zh <tipine@163.com>
2025-05-27 19:22:51 +0800

    debug

--- database/poi.go
+++ database/poi.go
@@ -7,17 +7,6 @@ import (
 	"github.com/jianbo-zh/jydata/database/ent/poi"
 )
 
-type PoiType int
-
-const (
-	PoiType_Unknown         PoiType = 0
-	PoiType_ParkingSpot     PoiType = 1 // 停车点
-	PoiType_TouristSpot     PoiType = 2 // 景点
-	PoiType_Toilet          PoiType = 3 // 厕所
-	PoiType_Shop            PoiType = 4 // 商铺
-	PoiType_EntranceAndExit PoiType = 5 // 出入口
-)
-
 func (m *Database) GetScenicAreaPois(ctx context.Context, scenicAreaID int, levels []int) ([]*ent.Poi, error) {
 	query := m.MainDB().Poi.Query().Where(poi.ScenicAreaIDEQ(scenicAreaID))
 	if len(levels) > 0 {
